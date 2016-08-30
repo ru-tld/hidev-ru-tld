e737a3930ca38aadf617f33e3141def562ead33a
Andrii Vasyliev <sol@hiqdev.com>
2016-08-30 13:01:42 +0000

    + hidev config

---
+++ src/config/hidev.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * RU-TLD vendor configuration for HiDev
+ *
+ * @link      https://github.com/ru-tld/hidev-ru-tld
+ * @package   hidev-ru-tld
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015-2016, RU-TLD (http://ru-tld.ru/)
+ */
+
+return [
+    'components' => [
+        'config' => [
+            'include' => [
+                '@rutld/hidev/rutld/config/goals.yml',
+            ],
+        ],
+    ],
+];
